8dfcb2de0a98b85a124f73742f9bed1e14df17ea
Dênis Augusto Luft <denisluft8@gmail.com>
2023-05-16 18:41:16 -0300

    form done and styled and map implemented

--- src/pages/Contact/Contact.styled.ts
+++ src/pages/Contact/Contact.styled.ts
@@ -9,7 +9,7 @@ export const ContactSection = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
-  color: #fff;
+  color: #ddd;
 `;
 
 export const ContactContainer = styled.div<ContactContainerProps>`
@@ -18,7 +18,11 @@ export const ContactContainer = styled.div<ContactContainerProps>`
   width: 38%;
   padding: 12px;
   & > h2 {
-    margin-top: 500px;
+  }
+  & > iframe {
+    border: none;
+    height: 480px;
+    width: 100%;
   }
 `;
 
@@ -26,7 +30,99 @@ export const IconContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
 
-  & > a > img {
-    width: 100px;
+  & > a {
+    cursor: pointer;
+    & > img {
+      width: 100px;
+      &:hover {
+        transform: scale(1.1);
+        transition: 0.5s ease;
+      }
+    }
+  }
+`;
+
+export const ContactForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  align-items: center;
+
+  & > label {
+    border-width: 0 !important;
+    clip: rect(1px, 1px, 1px, 1px) !important;
+    height: 1px !important;
+    overflow: hidden !important;
+    padding: 0 !important;
+    position: absolute !important;
+    white-space: nowrap !important;
+    width: 1px !important;
+  }
+
+  & > input {
+    background-color: transparent;
+    border: none;
+    color: white;
+    border-bottom: 2px solid #ff8922;
+    height: 40px;
+    font-size: 20px;
+    outline: none;
+    padding-left: 10px;
+    transition: 0.5s ease;
+    width: 350px;
+
+    &::placeholder {
+      color: #eee;
+      font-size: 20px;
+    }
+
+    &:focus {
+      width: 380px;
+    }
+  }
+
+  & > textarea {
+    background-color: transparent;
+    border: none;
+    border-bottom: 2px solid #ff8922;
+    color: white;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-size: 20px;
+    outline: none;
+    height: 40px;
+    padding: 10px 0px 0px 10px;
+    resize: none;
+    transition: 0.5s ease;
+    width: 350px;
+
+    &::-webkit-scrollbar {
+      width: 4px;
+    }
+
+    /* Track */
+    &::-webkit-scrollbar-track {
+      background: #034781;
+      width: 2px;
+    }
+
+    /* Handle */
+    &::-webkit-scrollbar-thumb {
+      background: #ff8922;
+    }
+
+    /* Handle on hover */
+    &::-webkit-scrollbar-thumb:hover {
+      background: #f97400;
+    }
+
+    &::placeholder {
+      color: #eee;
+      font-size: 20px;
+    }
+
+    &:focus {
+      height: 140px;
+      width: 380px;
+    }
   }
 `;
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,8 +1,9 @@
+import { ChangeEvent, useState } from "react";
 import { constructionSilhouette2, insta, whats } from "../../assets";
 import { SectionName } from "../../components";
 import {
-  ContactBgImg,
   ContactContainer,
+  ContactForm,
   ContactSection,
   IconContainer,
 } from "./Contact.styled";
@@ -11,6 +12,11 @@ export const Contact = () => {
   return (
     <ContactSection>
       <ContactContainer bgColor="#022a4d">
+        <h2>Veja algumas de nossas obras:</h2>
+        <iframe
+          src="https://www.google.com/maps/d/embed?mid=1wALpnsg7zPP2KYq7dKLaL6AN6PknouY&ehbc=2E312F"
+          
+        ></iframe>
         <h2>Fale conosco pelas nossas redes:</h2>
         <IconContainer>
           <a>
@@ -23,8 +29,23 @@ export const Contact = () => {
       </ContactContainer>
       <ContactContainer bgColor="#022a4d">
         <SectionName text={"Entre em Contato"} textColor={"#fff"} />
+        <h3>
+          Ou preencha o formulário abaixo e assim que possível entraremos em
+          contato:
+        </h3>
+        <ContactForm>
+          <label htmlFor="name">Nome:</label>
+          <input id="name" name="name" placeholder="Nome"></input>
+          <label htmlFor="email">Email:</label>
+          <input id="email" name="email" placeholder="Email"></input>
+          <label htmlFor="message">Mensagem</label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Mensagem"
+          ></textarea>
+        </ContactForm>
       </ContactContainer>
     </ContactSection>
   );
 };
-
